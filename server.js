8330675ff08ce58ff3c6e07d153efe9ee75102bf
Praise James <66879837+DrPraze@users.noreply.github.com>
2023-04-04 14:50:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ function generateC(text){
 
 
 app.post('/generate', function(req, res, next){
-	const { prompt } = req.query.text;
+	const { prompt } = req.query.prompt;
 	const { c, extra } = generateC(prompt);
 	return res.render('index.ejs', {"comic":c});
 });
