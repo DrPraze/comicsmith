d55d4caef3ef6bb922e36afc2decf6c743423069
Praise James <66879837+DrPraze@users.noreply.github.com>
2023-04-05 13:10:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const sharp = require('sharp');
 const FormData = require('form-data');
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
